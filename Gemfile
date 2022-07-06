ded5fb28478394fea7adc063a82d404c5f7bc321
Alexey K <alexcrpts@gmail.com>
2022-07-06 12:13:39 +0300

    refactor: merge all changes alltogether from one draft
    * Bundle app.
    
    * Namespace app.
    
    * Take multiple backups instead of just two as input.
    
    * Battletest keword arguments(first time using them in real app)
    
    * Make complete overhaul how categories and their subscriptions are
      retrieved from all imported backups and applied to merged backup.
    
    * Do not use 'selected'. I have not noticed it's used anywhere, app
      works fine with backups without it.

---
+++ Gemfile
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+source "https://rubygems.org"
+
+gem 'activesupport', require: ['active_support/core_ext/string/filters',
+                               'active_support/core_ext/string/inflections',
+                               'active_support/core_ext/hash/keys']
+gem 'json'
--- ft_sam.rb
+++ ft_sam.rb
@@ -1,96 +1,95 @@
-require 'json'
+require 'rubygems'
+require 'bundler'
+Bundler.setup(:default)
+Bundler.require(:default)
 
-class FreeTubeDBMerger
-  def self.merge
-    FreeTubeDBMerger.new(*ARGV).run
-  end
+module FreeTubeTools
+  class SubscriptionMerger
+    def self.run
+      dbs = ARGV
+      export_lists = dbs.map { |db| List.new(SubscriptionMerger.serialize(db)) }
+      import_list = convert(export_lists, List.new)
+      save(import_list)
+    end
 
-  def self.serialize(db)
-    file = File.open("./#{db}", 'r', &:readlines)
-    file.map { |line| JSON.parse(line) }
-  end
+    def self.serialize(db)
+      file = File.open("./#{db}", 'r', &:readlines)
+      file.map! { |line| JSON.parse(line) }
+      file.map { |hash| hash.deep_transform_keys { |key| key.underscore.to_sym } }
+    end
 
-  def initialize(db1, db2)
-    @list1, @list2 = [db1, db2].map { |db| List.new(FreeTubeDBMerger.serialize(db)) }
-    @unified_list = List.new([])
-    @unified_subs = []
-  end
+    def self.convert(export_lists = [], import_list)
+      export_lists.each do |e_list|
+        e_list.categories.each do |e_cat|
+          import_cat_matched = import_list.categories.find { |i_cat| i_cat.name == e_cat.name }
 
-  def run
-    unify_categories
-    unify_subs
-    save_to_file
-  end
+          if import_cat_matched
+            import_cat_matched.subscriptions = import_cat_matched.subscriptions | e_cat.subscriptions
+          else
+            new_cat = List::Category.new(name: e_cat.name,
+                                         _id: e_cat.id,
+                                         bg_color: e_cat.bg_color,
+                                         text_color: e_cat.text_color,
+                                         subscriptions: e_cat.subscriptions)
+            import_list.categories.push(new_cat)
+          end
+        end
+      end
+      import_list
+    end
 
-  private
-
-  def unify_categories
-    categories1 = @list1.categories
-    categories2 = @list2.categories
-    unified_categories = (categories1 + categories2).uniq
-    @unified_list.categories = unified_categories
-  end
-
-  def unify_subs
-    @unified_list.categories.map do |u_c|
-      cat1 = @list1.categories.find(proc { List::Category.new({}) }) { |c| c.name == u_c.name }
-      cat2 = @list2.categories.find(proc { List::Category.new({}) }) { |c| c.name == u_c.name }
-      u_c.subscriptions = (cat1.subscriptions + cat2.subscriptions).uniq
+    def self.save(list)
+      date = Time.now.strftime('%Y-%m-%d_%H%M')
+      file = File.open("./ft_merged_#{date}.db", 'w')
+      categories = list.categories.map { |c| JSON.generate(c.to_h) }
+      file.write categories.join("\n") << "\n"
     end
   end
 
-  def save_to_file
-    date = Time.now.strftime('%Y-%m-%d_%H%M')
-    file = File.new("./ft_merged_#{date}.db", 'w')
-    @unified_list.categories.map! { |category| JSON.generate(category.to_h) }
-    file.write @unified_list.categories.join("\n") << "\n"
-    file.close
-  end
-end
+  class List
+    attr_reader :categories
 
-class List
-  attr_accessor :categories
-
-  def initialize(categories)
-    @categories = categories.map { |category| Category.new(category) }
-  end
-
-  class Category
-    attr_accessor :subscriptions
-    attr_reader :name
-
-    def initialize(category)
-      @name = category['name']
-      @id = category['_id']
-      @bg_color = category['bgColor']
-      @text_color = category['textColor']
-      @subscriptions = category.fetch('subscriptions', []).map { |subscription| Subscription.new(subscription) }
+    def initialize(categories = [])
+      @categories = categories.map { |c| Category.new(**c) }
     end
 
-    def eql?(other)
-      @name == other.name
-    end
+    class Category
+      attr_accessor :subscriptions
+      attr_reader :name, :id, :bg_color, :text_color
 
-    def hash
-      @name.hash
-    end
+      def initialize(name: nil, _id: nil, bg_color: nil, text_color: nil, subscriptions: [])
+        @name = name
+        @id = _id
+        @bg_color = bg_color
+        @text_color = text_color
+        @subscriptions = subscriptions.map { |s| s.is_a?(Subscription) ? s : Subscription.new(**s) }
+      end
 
-    def to_h
-      { 'name' => @name,
-        '_id' => @id,
-        'bgColor' => @bg_color,
-        'textColor' => @text_color,
-        'subscriptions' => self.subscriptions.map(&:to_h) }
+      def eql?(other)
+        @name == other.name
+      end
+
+      def hash
+        @name.hash
+      end
+
+      def to_h
+        { 'name' => @name,
+          '_id' => @id,
+          'bgColor' => @bg_color,
+          'textColor' => @text_color,
+          'subscriptions' => self.subscriptions.map(&:to_h) }
+      end
     end
 
     class Subscription
       attr_reader :name
 
-      def initialize(subscriptions)
-        @id = subscriptions['id']
-        @name = subscriptions['name']
-        @thumbnail = subscriptions['thumbnail']
-        @selected = subscriptions['selected']
+      def initialize(id: nil, name: nil, thumbnail: nil, selected: nil)
+        @id = id
+        @name = name
+        @thumbnail = thumbnail
+        @selected = selected
       end
 
       def eql?(other)
@@ -103,11 +102,9 @@ class List
 
       def to_h
         atr = { 'name' => @name, 'id' => @id, 'thumbnail' => @thumbnail }
-        atr['selected'] = @selected unless @selected.nil?
-        atr
+        # atr.merge({ 'selected' => @selected }) unless @selected.nil?
       end
     end
   end
 end
-
-FreeTubeDBMerger.merge
+FreeTubeTools::SubscriptionMerger.run
